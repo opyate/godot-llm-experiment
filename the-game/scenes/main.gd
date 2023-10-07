3ba9620617126e10a9cedaef569fd7eb6a044c9d
Juan Uys <opyate@gmail.com>
2023-10-07 19:29:31 +0100

    part 7: 'run_completion' can't be found on GDLLM...

--- the-game/scenes/main.gd
+++ the-game/scenes/main.gd
@@ -1,1 +1,14 @@
 extends Node2D
+
+
+func _ready():
+	var llm = GDLLM.new()
+	
+	llm.connect("completion_generated", on_completion_generated)
+	
+	llm.run_completion("Hello, my name is ")
+	
+
+func on_completion_generated(_node: Node, completion_text: String):
+	print("Got completion:")
+	print(completion_text)
